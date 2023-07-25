ccf92c45603394d75a7792df643b2f57325c4e3c
unknown <onsjannet@gmail.com>
2023-07-25 19:10:22 +0100

    routing

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,12 +1,22 @@
 // Your dashboard-related controller functions
 
 // Example:
-exports.getDashboard = (req, res) => {
+  exports.getDashboard = (req, res) => {
     const data = { message: 'Welcome to the dashboard.' };
     res.json(data);
   };
   
   exports.updateDashboard = (req, res) => {
     res.json({ message: 'Dashboard updated successfully.' });
+  };
+
+  exports.getAdminDashboard = (req, res) => {
+    const data = { message: 'Welcome to the Admin dashboard.' };
+    res.json(data);
+  };
+
+  exports.getModeratorDashboard = (req, res) => {
+    const data = { message: 'Welcome to the Moderator dashboard.' };
+    res.json(data);
   };
   
